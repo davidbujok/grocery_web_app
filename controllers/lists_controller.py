bdbbed31eb727dd454c8969b50a12512d49288cd
davidbujok <davebujok@gmail.com>
2023-07-28 15:24:54 +0100

    Add search bar to list page, all individual item search

--- controllers/lists_controller.py
+++ controllers/lists_controller.py
@@ -2,6 +2,7 @@ from app import db
 from flask_sqlalchemy import SQLAlchemy
 from flask import Blueprint, redirect, render_template, request
 from models.list import List
+from models.item import Item
 lists_blueprint = Blueprint('lists', __name__)
 
 
@@ -12,5 +13,13 @@ def lists():
 
 @lists_blueprint.route('/lists/<id>')
 def show_list(id):
+    list = List.query.get(id) 
+    items = Item.query.all()
+    return render_template('/lists/show_list.jinja', list=list, items=items)
+
+@lists_blueprint.route('/lists/<id>/search', methods=['POST'])
+def search_item(id):
     list = List.query.get(id)
-    return render_template('/lists/show_list.jinja', list=list)
+    item_name_form = request.form['item']
+    item = db.session.query(Item).filter_by(name=item_name_form).first()
+    return render_template('/lists/search_item.jinja', list=list, item=item)
