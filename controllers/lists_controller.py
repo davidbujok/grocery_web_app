e4e4dc2c8fb4ed8c6adba70ddf375fffae2ffe11
davidbujok <davebujok@gmail.com>
2023-07-29 11:49:15 +0100

    fix: (search for individual items error)
    Refactoring to sqlalchemy orm querying 2.0
    introduced error when searching for item.

--- controllers/lists_controller.py
+++ controllers/lists_controller.py
@@ -1,53 +1,75 @@
 from app import db
 from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy import select
 from flask import Blueprint, redirect, render_template, request, url_for
 from models.list import List
 from models.item import Item
 from models.user_list import UserList
-from models.user import User
 lists_blueprint = Blueprint('lists', __name__)
 
 
 @lists_blueprint.route('/lists')
 def lists():
-    # lists = List.query.all()
-    lists = db.session.scalars(db.select(List).order_by(List.name)).all()
+    lists = db.session.execute(
+            db.select(List)
+            ).scalars()
     return render_template('/lists/lists.jinja', lists=lists) 
+
 
 @lists_blueprint.route('/lists/<id>')
 def show_list(id): 
-    # list = List.query.get(id) 
-    list = db.select(List).where(List.id == id)
-    items = Item.query.all()
+    list = db.session.scalar(
+            db.select(List)
+            .where(List.id == id)
+            )
+    items = db.session.scalars(
+            db.select(Item)
+            )
     on_list = db.session.scalars(
               db.select(UserList)
               .join(Item, UserList.item_list_id == Item.id)
               .where(UserList.user_list_id == id)
-              .order_by(Item.name.asc())  
+              .order_by(Item.name.asc())
               )
     return render_template('/lists/show_list.jinja', list=list, items=items, on_list=on_list)
 
+
 @lists_blueprint.route('/lists/<id>/search', methods=['GET', 'POST'])
 def search_item(id):
-    list = List.query.get(id)
     item_name_form = request.form['item']
-    item = db.session.query(Item).filter_by(name=item_name_form).first()
-    on_list = db.session.execute(
-              db.select(UserList)
-              .join(Item, UserList.item_list_id == Item.id)
-              .where(UserList.user_list_id == id)
-              .order_by(Item.name.asc())
-              ).scalars()
+    list = db.session.scalar(
+            db.select(List)
+            .where(List.id == id)
+            )
+    item = db.session.scalar(
+            db.select(Item)
+            .where(Item.name == item_name_form)
+            )
+    on_list = db.session.scalars(
+                db.select(UserList)
+                .join(Item, UserList.item_list_id == Item.id)
+                .where(UserList.user_list_id == id)
+                .order_by(Item.name.asc())
+                )
     return render_template('/lists/search_item.jinja', list=list, item=item, on_list=on_list)
+
 
 @lists_blueprint.route('/lists/<id>/add', methods=['POST'])
 def add_item(id):
-    list = List.query.get(id)
     item_name_form = request.form['item_name']
-    item = db.session.query(Item).filter_by(name=item_name_form).first()
+    list = db.session.scalar(
+            db.select(List)
+            .where(List.id == id)
+            )
+    item = db.session.scalar(
+            db.select(Item)
+            .where(Item.name == item_name_form)
+            )
     user_list = UserList(user_list_id = list.id, item_list_id=item.id)
     db.session.add(user_list)
     db.session.commit()
-    on_list = db.session.execute(db.select(UserList).where(UserList.user_list_id == id)).scalars()
+
+    on_list = db.session.scalars(
+                db.select(UserList)
+                .where(UserList.user_list_id == id)
+                )
     return render_template('/lists/search_item.jinja', list=list, item=item, on_list=on_list)
