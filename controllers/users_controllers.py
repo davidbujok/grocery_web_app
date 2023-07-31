3814e50885a43ae1a2a93b6c02bf9644dc457ef5
davidbujok <davebujok@gmail.com>
2023-07-31 14:58:12 +0100

    Add model category, user_layouts
    Create user_layout and model many to many
    relationship between category and store

--- controllers/users_controllers.py
+++ controllers/users_controllers.py
@@ -2,7 +2,9 @@ from flask import Flask, Blueprint, render_template, request, redirect, url_for
 from app import db
 from models.user import User
 from models.item import Item
+from models.category import Category  
 from models.list import List 
+from models.user_layouts import UserLayouts
 from models.store import Store
 from models.user_list import UserList
 
